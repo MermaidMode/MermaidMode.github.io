f5ef927f54e676b8f4c3b69b511c60ddb5dea1a9
MermaidMode <victoriacarolina100@gmail.com>
2018-05-11 11:06:06 -0400

    jquery updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	// $(".vignette").animate({boxShadow: "0, 0, 0 rgba(0,0,0,0)"}, "1000");
+
 	$(".dropdown-content").hide();
 
 	$(".dropdown").hover(function(){
@@ -7,4 +9,18 @@ $(document).ready(function(){
 		console.log("hello");
 	})
 
+	$(".dropdown").mouseover(function(){
+		$(this).children(".dropbtn").css("background-color", "rgba(0, 0, 0, .3)");
+		$(this).children(".dropbtn").css("border-radius", "15px");
+	})
+
+	$(".dropdown").mouseleave(function(){
+		$(this).children(".dropbtn").css("background-color", "rgba(0, 0, 0, 0)");
+	})
+
+
+
+
+
 })
+

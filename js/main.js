5e64945fd16f565142a6176038c98dcaa4384baa
MermaidMode <victoriacarolina100@gmail.com>
2018-05-11 11:56:01 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ $(document).ready(function(){
 
 	$(".dropdown").hover(function(){
 		$(this).children(".dropdown-content").slideToggle();
+		$(this).children(".dropdown-content").css("border-radius", "15px");
 		console.log("hello");
 	})
 
 	$(".dropdown").mouseover(function(){
 		$(this).children(".dropbtn").css("background-color", "rgba(0, 0, 0, .3)");
 		$(this).children(".dropbtn").css("border-radius", "15px");
+		$(this).children(".dropbtn").css("transition", "1s");
 	})
 
 	$(".dropdown").mouseleave(function(){
